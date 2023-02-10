d3ee736c911109f71e82886f2dcc47b7fad5cbc9
Lailson Santos <lbsantos10@stefanini.com>
2023-02-09 21:40:41 -0300

    Adicionada as rotas

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,15 +3,13 @@ import { ReactComponent as Logo } from 'assets/logo.svg';
 import Buscador from './Buscador';
 import { useState } from 'react';
 import Filtros from './Filtros';
-import Ordenador from './Ordenador/Ordeandor';
+import Ordenador from './Ordenador';
 import Itens from './Itens';
 
 export default function Cardapio() {
-
-  const [busca, setBusca] = useState("");
+  const [busca, setBusca] = useState('');
   const [filtro, setFiltro] = useState<number | null>(null);
-  const [ordenador, setOrdenador] = useState("")
-
+  const [ordenador, setOrdenador] = useState('');
   return (
     <main>
       <nav className={styles.menu}>
@@ -32,5 +30,5 @@ export default function Cardapio() {
         <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
       </section>
     </main>
-  )
+  );
 }
